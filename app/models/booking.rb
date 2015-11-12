973dc26ef28da5c767e0c88d464ea2f8c6c35adc
Edouard Sartorius <edouardsartorius@gmail.com>
2015-11-12 14:40:21 +0100

    Add validates presence of in booking model

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -3,4 +3,5 @@ class Booking < ActiveRecord::Base
   belongs_to :client, class_name: "User", foreign_key: "client_id"
   belongs_to :guide, class_name: "User", foreign_key: "guide_id"
 
+  validates_presence_of :client, :owner, :starting_on, :ending_on, :status
 end
